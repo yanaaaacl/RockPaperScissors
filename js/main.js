967b150a4d19b8dbc40153bbcc0b48c2cfc0f6e9
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-09-18 17:53:01 +0300

    interface

--- js/main.js
+++ js/main.js
@@ -1,109 +1,95 @@
+let btnOne = document.querySelector('.btn__rock');
+let btnTwo = document.querySelector('.btn__paper');
+let btnThree = document.querySelector('.btn__scissor');
 
-function getComputerChoice() {
-    let arrPlay = [];
-    for (let i = 0; i < 5; i++) {
+let user = document.querySelector('.footer__inner-user');
+let computer = document.querySelector('.footer__inner-computer');
+
+let countOne = document.querySelector('.count__One');
+let countTwo = document.querySelector('.count__Two');
+
+let counterUser = 0;
+let counterComputer = 0;
+
+
+
+
+function rock(){
+let arr = ['Камень', 'Ножницы', 'Бумага'];
+let randomIndex = Math.floor(Math.random() * arr.length);
+let word = arr[randomIndex];
+for (let i = 0; i < 1; i++){
+    if (counterUser == 5 || counterComputer == 5){
+        alert('Game over');
+        break;
+    } else if (word == 'Камень') {
+        countOne.textContent = counterUser;
+        countTwo.textContent = counterComputer;
+        console.log(word);
+    } else if (word == 'Бумага') {
+        counterComputer += 1;
+        countTwo.textContent = counterComputer;
+        console.log(word);
+    } else if (word == 'Ножницы') {
+        counterUser += 1;
+        countOne.textContent = counterUser;
+        console.log(word);
+    }   
+}
+}
+
+function paper(){
+    let arr = ['Камень', 'Ножницы', 'Бумага'];
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    let word = arr[randomIndex];
+    for (let i = 0; i < 1; i++){
+        if (counterUser == 5 || counterComputer == 5){
+            alert('Game over');
+            break;
+        } else if (word == 'Бумага') {
+            countOne.textContent = counterUser;
+            countTwo.textContent = counterComputer;
+            console.log(word);
+        } else if (word == 'Ножницы') {
+            counterComputer += 1;
+            countTwo.textContent = counterComputer;
+            console.log(word);
+        } else if (word == 'Камень') {
+            counterUser += 1;
+            countOne.textContent = counterUser;
+            console.log(word);
+        }   
+    }
+    }
+
+    function scissor(){
         let arr = ['Камень', 'Ножницы', 'Бумага'];
         let randomIndex = Math.floor(Math.random() * arr.length);
-        arrPlay.push(arr[randomIndex]);
-
-    }
-    return arrPlay;
-};
-
-let getComputerAnswer = getComputerChoice();
-
-
-// просит у пользователя ввести пять раз слово, потом переписывает это слово в нужный регистр и записывает все эти слова в массив arrwords
-
-
-
-function time() {
-    let arrWords = [];
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Введите камень, ножницы или бумага:');
-        let result = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-        arrWords.push(result);
-    }
-    return arrWords;
-}
-
-let times = time();
-
-
-
-function roundGame(times, getComputerAnswer) {
-    let counterUser = 0;
-let counterComputer = 0;
-    for (let i = 0; i < 5; i++) {
-        let elem = times[i];
-        let elemTwo = getComputerAnswer[i];
-        for (let j = 0; j < 1; j++) {
-
-
-            if ((elem == 'Бумага') && (elemTwo == 'Камень')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
-                counterUser += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log ('Вы выиграли в этом раунде! Бумага бьет камень');
-            } else if ((elem == 'Камень') && (elemTwo == 'Бумага')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
+        let word = arr[randomIndex];
+        for (let i = 0; i < 1; i++){
+            if (counterUser == 5 || counterComputer == 5){
+                alert('Game over');
+                break;
+            } else if (word == 'Ножницы') {
+                countOne.textContent = counterUser;
+                countTwo.textContent = counterComputer;
+                console.log(word);
+            } else if (word == 'Камень') {
                 counterComputer += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log('Вы проиграли в этом раунде! Бумага бьет камень');
-            } else if ((elem == 'Ножницы') && (elemTwo == 'Бумага')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
+                countTwo.textContent = counterComputer;
+                console.log(word);
+            } else if (word == 'Бумага') {
                 counterUser += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log ('Вы выиграли в этом раунде! Бумагу режут ножницы');
-
-            } else if ((elem == 'Бумага') && (elemTwo == 'Ножницы')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
-                counterComputer += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log ('Вы проиграли в этом раунде! Бумагу режут ножницы');
-
-            } else if ((elem == 'Камень') && (elemTwo == 'Ножницы')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
-                counterUser += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log ('Вы выиграли в этом раунде! Камень точет ножницы');
-
-            } else if ((elem == 'Ножницы') && (elemTwo == 'Камень')) {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
-                counterComputer += 1;
-                console.log('Счет: ' + counterComputer + ' : ' + counterUser);
-                console.log ('Вы проиграли в этом раунде! Камень точет ножницы');
-
-            } else {
-                console.log(elemTwo, ' - Ход компьютера');
-                console.log(elem, ' - Ваш ход');
-                console.log ('Ничья');
-            }
-
-
+                countOne.textContent = counterUser;
+                console.log(word);
+            }   
         }
-    }
+        }
 
-    if ((counterComputer > counterUser) && (counterComputer - counterUser < 5)) {
-        console.log('Ты проиграл! С перевесом в: ' + (counterComputer - counterUser) + ' балла');
-    } else if ((counterComputer > counterUser) && (counterComputer - counterUser == 1)) {
-        console.log('Ты проиграл! С перевесом в: ' + (counterComputer - counterUser) + ' балл');
-    }else if ((counterUser > counterComputer) && (counterUser - counterComputer < 5)) {
-        console.log('Ты победил! С перевесом в: ' + (counterUser - counterComputer) + ' балла');
-    }else if ((counterUser > counterComputer) && (counterUser - counterComputer == 1)) {
-        console.log('Ты победил! С перевесом в: ' + (counterUser - counterComputer) + ' балл');
-    }else{
-        console.log('Ничья');
-    }
-}
+btnOne.addEventListener('click', rock);
+btnTwo.addEventListener('click', paper);
+btnThree.addEventListener('click', scissor);
 
-roundGame(times, getComputerAnswer);
 
 
 
